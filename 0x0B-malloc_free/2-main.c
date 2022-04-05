636ed370a2c7e21d30dfe80c3c530d2c53a87961
root <nancymuthee>
2022-04-05 15:00:08 -0700

    2

--- 0x0B-malloc_free/2-main.c
+++ 0x0B-malloc_free/2-main.c
@@ -1,42 +1,22 @@
-#include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
- *  * main - check the code for ALX School students.
- *   *
- *    * Return: Always 0.
- *     */
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-		char *s;
+    char *s;
 
-			s = str_concat("Hello", NULL);
-				if (s == NULL)
-						{
-									printf("failed\n");
-											return (1);
-												}
-					printf("%s\n", s);
-						free(s);
-
-						    s = str_concat(NULL, "Hello");
-						    	if (s == NULL)
-									{
-												printf("failed\n");
-														return (1);
-															}
-								printf("%s\n", s);
-									free(s);
-
-									    s = str_concat(NULL, NULL);
-									    	if (s == NULL)
-												{
-															printf("failed\n");
-																	return (1);
-																		}
-											printf("%s\n", s);
-												free(s);
-
-													return (0);
+    s = str_concat("Betty ", "Holberton");
+    if (s == NULL)
+    {
+        printf("failed\n");
+        return (1);
+    }
+    printf("%s\n", s);
+    free(s);
+    return (0);
 }
