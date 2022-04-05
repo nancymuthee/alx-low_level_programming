0ad00c9d3693b45ee1382fc025f8ad48b03b04a6
root <nancymuthee>
2022-04-05 14:55:36 -0700

    2

--- 0x0B-malloc_free/2-main.c
+++ 0x0B-malloc_free/2-main.c
@@ -9,15 +9,34 @@
  *     */
 int main(void)
 {
-	    char *s;
+		char *s;
 
-	        s = str_concat("Betty ", "Holberton");
-		    if (s == NULL)
-			        {
-					        printf("failed\n");
-						        return (1);
-							    }
-		        printf("%s\n", s);
-			    free(s);
-			        return (0);
+			s = str_concat("Hello", NULL);
+				if (s == NULL)
+						{
+									printf("failed\n");
+											return (1);
+												}
+					printf("%s\n", s);
+						free(s);
+
+						    s = str_concat(NULL, "Hello");
+						    	if (s == NULL)
+									{
+												printf("failed\n");
+														return (1);
+															}
+								printf("%s\n", s);
+									free(s);
+
+									    s = str_concat(NULL, NULL);
+									    	if (s == NULL)
+												{
+															printf("failed\n");
+																	return (1);
+																		}
+											printf("%s\n", s);
+												free(s);
+
+													return (0);
 }
