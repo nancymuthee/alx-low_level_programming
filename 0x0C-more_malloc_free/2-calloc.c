980b1cef05f0eeb3cd5ae4d85c44f4aae3047ee1
root <nancymuthee>
2022-04-07 15:06:12 -0700

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,18 +9,18 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-char *arr;
+char *p;
 unsigned int i;
 
 if (nmemb == 0 || size == 0)
 return (NULL);
 
-arr = malloc(nmemb * size);
-if (arr == NULL)
+p = malloc(nmemb * size);
+if (p == NULL)
 return (NULL);
 
 for (i = 0; i < (nmemb * size); i++)
-arr[i] = 0;
+p[i] = 0;
 
-return (arr);
+return (p);
 }
